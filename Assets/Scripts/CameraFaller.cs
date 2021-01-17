b1dbb4d0862d50a7899d91dddaa16fcf6eaae909
ziyagngr <gungorziya96@gmail.com>
2021-01-17 11:47:33 +0300

    camtrigger

---
+++ Assets/Scripts/CameraFaller.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFaller : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = .3f;
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target.position.y > transform.position.y)
+        {
+            Vector3 newPos = new Vector3(transform.position.x, target.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, newPos, smoothSpeed);
+
+        }
+    }
+}
