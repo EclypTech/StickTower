f53c472fedcdfcc6d9d3b60bb8b7f69797277d5d
ziyagngr <gungorziya96@gmail.com>
2021-01-24 21:48:09 +0300

    movement gg

--- Assets/Scripts/PlatformJumper.cs
+++ Assets/Scripts/PlatformJumper.cs
@@ -10,13 +10,19 @@ public class PlatformJumper : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
-        if (rb != null)
+        if(collision.relativeVelocity.y <= 0f)
         {
-            Vector2 velocity = rb.velocity;
-            velocity.y = jumpForce;
-            rb.velocity = velocity;
+            Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                Vector2 velocity = rb.velocity;
+                velocity.y = jumpForce;
+                rb.velocity = velocity;
+            }
         }
+
+
+        
     }
 
 
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+	public int movementSpeed;
+	float moveInput;
+	Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start()
 	{
-
+		rb = GetComponent<Rigidbody2D>();
 	}
 
 
 	// Update is called once per frame
 	void FixedUpdate()
 	{
-
+		rb.velocity = new Vector2(moveInput * movementSpeed, rb.velocity.y);
 	}
 
+	public void MoveRight()
+    {
+		moveInput = 1;
+    }
+
+	public void MoveLeft()
+	{
+		moveInput = -1;
+	}
+
+	public void MoveStop()
+	{
+		moveInput = 0;
+	}
 }
