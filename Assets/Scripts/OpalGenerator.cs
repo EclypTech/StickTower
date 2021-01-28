015b7b72ee5c1c96366f7fbbe7b2e456669c83c5
ziyagngr <gungorziya96@gmail.com>
2021-01-29 02:20:00 +0300

    amk

--- Assets/Scripts/OpalGenerator.cs
+++ Assets/Scripts/OpalGenerator.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class OpalGenerator : MonoBehaviour
 {
     public GameObject OpalPrefab;        // Define OpalPrefab to the script.
-    public float OpalRange = 12;           // Opal generate range. Must be a multiple of 3. BUT shouldnt be multiple of Staminarange.
+    public float OpalRange = 93;           // Opal generate range. Must be a multiple of 3. BUT shouldnt be multiple of Staminarange.
     public int OpalNum = 0; 
 
 
@@ -17,7 +17,7 @@ public class OpalGenerator : MonoBehaviour
 
         if (findgenerator.spawnPosition.y == OpalRange)   // If Platform spawn position equal to stamina range...
         {
-            OpalRange += findgenerator.rangeY * 6;                                                 // Increase stamina spawn position.
+            OpalRange += findgenerator.rangeY * 62;                                                 // Increase stamina spawn position.
             findgenerator.spawnPosition.y += 0.6f;                                            // Stamina spawn under the current platform.
             Instantiate(OpalPrefab, findgenerator.spawnPosition, Quaternion.identity);     // Spawn staminaprefab.
             findgenerator.spawnPosition.y -= 0.6f;                                            // Increase position again for current platform.
@@ -33,7 +33,7 @@ public class OpalGenerator : MonoBehaviour
 
         if (findgenerator.spawnPosition.y == OpalRange)   // If Platform spawn position equal to stamina range...
         {
-            OpalRange += findgenerator.rangeY * 6;                                                                // Increase stamina spawn position.
+            OpalRange += findgenerator.rangeY * 62;                                                                // Increase stamina spawn position.
             findgenerator.spawnPosition.y += 0.6f;                                          // Stamina spawn under the current platform.
             Instantiate(OpalPrefab, findgenerator.spawnPosition, Quaternion.identity);      // Spawn staminaprefab.
             findgenerator.spawnPosition.y -= 0.6f;                                          // Increase position again for current platform.
