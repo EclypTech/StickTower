53639ccf1b94ecea499d9f514bfa73e4a13f0f57
ziyagngr <gungorziya96@gmail.com>
2021-02-07 20:22:36 +0300

    destvult

--- Assets/Scripts/DestroyPlayer.cs
+++ Assets/Scripts/DestroyPlayer.cs
@@ -7,11 +7,12 @@ public class DestroyPlayer : MonoBehaviour
     public GameObject DestroyerPoint;  // Define Game object.
     public GameObject GameOverCanvas;
 
-
     void Start()
     {
         DestroyerPoint = GameObject.Find("DestroyerPoint");
         //When game start, find destroyer point which means tracking lower from the camera.
+
+        
 
     }
 
@@ -27,17 +28,21 @@ public class DestroyPlayer : MonoBehaviour
 
      public void OneMoreChance()
     {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
         transform.eulerAngles = new Vector2(0, 0);
 
         GameObject findnewpos = GameObject.Find("NewPosPlayer");
-        transform.position = new Vector3(-1 , findnewpos.transform.position.y + 8 , -1);
+        transform.position = new Vector3(-1 , findnewpos.transform.position.y + 3 , -1);
 
         GameObject findstamina = GameObject.Find("StaminaBar");
         StaminaBar findbar = findstamina.GetComponent<StaminaBar>();
         findbar.slider.value = findbar.slider.maxValue;
-
-        Time.timeScale = 1;
         GameOverCanvas.SetActive(false);
+        Time.timeScale = 1;
+        rb.velocity = new Vector2(0 , 10);
+        GetComponent<BoxCollider2D>().enabled = true;
+
 
     }
 
--- Assets/Scripts/EnemyInteraction.cs
+++ Assets/Scripts/EnemyInteraction.cs
@@ -19,9 +19,12 @@ public class EnemyInteraction : MonoBehaviour
     {
         if (collision.transform.tag == "enemy")
         {
+
             transform.eulerAngles = new Vector2(180, 0);
             animator.SetTrigger("IsDead");
             GetComponent<BoxCollider2D>().enabled = false;
+            Destroy(collision.gameObject);
         }
     }
+
 }
