23f59fafb61b79f6c627a0db70284ef6fb4abeea
ziyagngr <gungorziya96@gmail.com>
2021-02-07 04:05:39 +0300

    onemorechance yarım kaldı

--- Assets/Scripts/DestroyPlayer.cs
+++ Assets/Scripts/DestroyPlayer.cs
@@ -12,6 +12,7 @@ public class DestroyPlayer : MonoBehaviour
     {
         DestroyerPoint = GameObject.Find("DestroyerPoint");
         //When game start, find destroyer point which means tracking lower from the camera.
+
     }
 
     void Update()
@@ -19,9 +20,24 @@ public class DestroyPlayer : MonoBehaviour
         if (transform.position.y < DestroyerPoint.transform.position.y)
         // If player has a lower vertical value from the lower limit of camera..
         {
-            //Destroy(gameObject); // Destroy player.
             GameOverCanvas.SetActive(true);
         }
+
+    }
+
+     public void OneMoreChance()
+    {
+        transform.eulerAngles = new Vector2(0, 0);
+
+        GameObject findnewpos = GameObject.Find("NewPosPlayer");
+        transform.position = new Vector3(-1 , findnewpos.transform.position.y + 8 , -1);
+
+        GameObject findstamina = GameObject.Find("StaminaBar");
+        StaminaBar findbar = findstamina.GetComponent<StaminaBar>();
+        findbar.slider.value = findbar.slider.maxValue;
+
+        Time.timeScale = 1;
+        GameOverCanvas.SetActive(false);
 
     }
 
