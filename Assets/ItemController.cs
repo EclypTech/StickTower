3760e1ece37f4d4c014489694994a431fced0b62
aybrkacr <ayberkacar96@hotmail.com.tr>
2021-02-09 07:30:14 +0300

    sda

--- Assets/ItemController.cs
+++ Assets/ItemController.cs
@@ -10,31 +10,52 @@ public class ItemController : MonoBehaviour
     [SerializeField] private GameObject helmet;
 
     [SerializeField] private int itemSelectNum;
+    
+    [SerializeField] private GameObject RockPrefab;
+    [SerializeField] private GameObject SmashedRockPrefab;
 
-    // Start is called before the first frame update
+    [SerializeField] private GameObject VulturePrefab;
+
+    [SerializeField] private GameObject Machette;
+
+    public float maxForce;
+    public float minForce;
+    public float radius;
+
+    private Vector3 smashedVector3 = new Vector3();
+    private Vector2 forceDirection = new Vector2();
+
+
     void Start()
     {
         itemSelectNum = PlayerPrefs.GetInt("itemnum");
+        
 
         if(itemSelectNum == 1)
         {
             GetItem(false, true, false, false);
+            TagChanger("enemy", "enemy");
         }
         else if(itemSelectNum == 2)
         {
             GetItem(false, false, true, false);
+            TagChanger("enemy", "NotEnemy");
         }
         else if(itemSelectNum == 3)
         {
             GetItem(true, false, false, true);
+            TagChanger("NotEnemy","enemy");
+            
+
         }
         else
         {
             GetItem(true, false, false, false);
+            TagChanger("enemy", "enemy");
         }
     }
 
-    // Update is called once per frame
+    
     private void GetItem(bool b_Pick , bool b_Grapling, bool b_Machette , bool b_Helmet)
     {
         pick.SetActive(b_Pick);
@@ -42,4 +63,48 @@ public class ItemController : MonoBehaviour
         machette.SetActive(b_Machette);
         helmet.SetActive(b_Helmet);
     }
+
+    private void TagChanger(string rock, string vulture)
+    {
+        RockPrefab.tag = rock;
+        VulturePrefab.tag = vulture;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.transform.tag == "NotEnemy" && itemSelectNum == 3) {
+
+            smashedVector3 =  collision.gameObject.transform.position;
+
+            GameObject smashedRock = Instantiate(SmashedRockPrefab, smashedVector3, Quaternion.identity);
+
+            Destroy(collision.gameObject);
+
+            Debug.Log(smashedRock.transform.childCount);
+
+            for(int i = 0; i < smashedRock.transform.childCount; i++)
+            {
+                forceDirection = new Vector2(Random.Range(minForce, maxForce), Random.Range(minForce, maxForce));
+                
+                var rb = smashedRock.transform.GetChild(i).GetComponent<Rigidbody2D>();
+                
+                if (rb != null)
+                {
+                    rb.AddForce(forceDirection);
+                }
+            }
+        }
+
+        if(collision.transform.tag == "NotEnemy" && itemSelectNum == 2)
+        {
+            collision.gameObject.transform.eulerAngles = new Vector2(180, 0) ;
+            collision.gameObject.GetComponent<Animator>().enabled = false;
+            collision.gameObject.AddComponent<Rigidbody2D>();
+            
+
+        }
+
+    }
+
 }
